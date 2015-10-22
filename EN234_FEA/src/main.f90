1312c124ad4912a5bd190d7de7d7fdcc2c49d9b9
Pinkesh <Pinkesh@PM.engin.brown.edu>
2015-10-22 05:35:49 -0400

    B-bar method

--- EN234_FEA/src/main.f90
+++ EN234_FEA/src/main.f90
@@ -36,9 +36,13 @@ program en234fea
 !  Homework 3
 !  Basic 2 element test (one or two elements)
 !  infil = './input_files/linear_elastic_2d.in'
-!  open (unit = IOR, file = infil, status = 'old', ERR=500)
+! open (unit = IOR, file = infil, status = 'old', ERR=500)
 !  outfil = './Output_files/linear_elastic_2d.out'
-!  open (UNIT = IOW, FILE = outfil, STATUS = 'unknown', ERR=500)
+! open (UNIT = IOW, FILE = outfil, STATUS = 'unknown', ERR=500)
+  infil = './input_files/linear_elastic_2d_Bbar.in'
+   open (unit = IOR, file = infil, status = 'old', ERR=500)
+  outfil = './Output_files/linear_elastic_2d_Bbar.out'
+  open (UNIT = IOW, FILE = outfil, STATUS = 'unknown', ERR=500)
 ! Homework 3, Basic 2D linear elasticity with different element types.
 !  infil = './input_files/holeplate_2d_tri3.in'
 !  open (unit = IOR, file = infil, status = 'old', ERR=500)
@@ -56,14 +60,22 @@ program en234fea
 !  open (unit = IOR, file = infil, status = 'old', ERR=500)
 !  outfil = './Output_files/holeplate_2d_quad8.out'
 !  open (UNIT = IOW, FILE = outfil, STATUS = 'unknown', ERR=500)
+! infil = './input_files/holeplate_3d_Bbar.in'
+! open (unit = IOR, file = infil, status = 'old', ERR=500)
+! outfil = './Output_files/holeplate_3d_Bbar.out'
+! open (UNIT = IOW, FILE = outfil, STATUS = 'unknown', ERR=500)
+!  infil = './input_files/holeplate_3d.in'
+! open (unit = IOR, file = infil, status = 'old', ERR=500)
+! outfil = './Output_files/holeplate_3d.out'
+! open (UNIT = IOW, FILE = outfil, STATUS = 'unknown', ERR=500)
 !
 
 !
 !  Homework 4, crack tip elements and the J integral
-  infil = './input_files/crack_tri6.in'
-  open (unit = IOR, file = infil, status = 'old', ERR=500)
-  outfil = './Output_files/crack_tri6.out'
-  open (UNIT = IOW, FILE = outfil, STATUS = 'unknown', ERR=500)
+!  infil = './input_files/crack_tri6.in'
+!  open (unit = IOR, file = infil, status = 'old', ERR=500)
+! outfil = './Output_files/crack_tri6.out'
+!  open (UNIT = IOW, FILE = outfil, STATUS = 'unknown', ERR=500)
 
 !  Homework 5, small-strain B bar element - test with same files as in HW3, but
 !  try approaching incompressible limit by making Poisson's ratio close to 0.5
--- EN234_FEA/user_codes/src/user_element.f90
+++ EN234_FEA/user_codes/src/user_element.f90
@@ -1,4 +1,3 @@
-!
 !     Subroutines to assemble element level matrices for FEA analysis
 
 !=========================== subroutine user_element_stiffness ===================
@@ -64,6 +63,14 @@ subroutine user_element_static(lmn, element_identifier, n_nodes, node_property_l
     n_state_variables, initial_state_variables, &                                                ! Input variables
     updated_state_variables,element_stiffness,element_residual, fail)      ! Output variables
 
+    else if ( element_identifier == 1002 ) then              ! Basic fully integrated 3D linear elastic element
+
+        call el_linelast_3d_Bbar(lmn, element_identifier, n_nodes, node_property_list, &           ! Input variables
+    n_properties, element_properties, element_coords, length_coord_array, &                      ! Input variables
+    dof_increment, dof_total, length_dof_array, &                                                ! Input variables
+    n_state_variables, initial_state_variables, &                                                ! Input variables
+    updated_state_variables,element_stiffness,element_residual, fail)      ! Output variables
+
 
     else if ( element_identifier ==0) then           ! Stub for a new element
   
@@ -76,6 +83,14 @@ subroutine user_element_static(lmn, element_identifier, n_nodes, node_property_l
    else if ( element_identifier ==101) then           ! Stub for a new element
   
     call el_linelast_2Dbasic(lmn, element_identifier, n_nodes, node_property_list, &           ! Input variables
+    n_properties, element_properties, element_coords, length_coord_array, &                      ! Input variables
+    dof_increment, dof_total, length_dof_array, &                                                ! Input variables
+    n_state_variables, initial_state_variables, &                                                ! Input variables
+    updated_state_variables,element_stiffness,element_residual, fail)      ! Output variables
+
+    else if ( element_identifier ==102) then           ! Stub for a new element
+
+    call el_linelast_2D_Bbar(lmn, element_identifier, n_nodes, node_property_list, &           ! Input variables
     n_properties, element_properties, element_coords, length_coord_array, &                      ! Input variables
     dof_increment, dof_total, length_dof_array, &                                                ! Input variables
     n_state_variables, initial_state_variables, &                                                ! Input variables
@@ -231,6 +246,15 @@ subroutine user_element_fieldvariables(lmn, element_identifier, n_nodes, node_pr
             n_field_variables,field_variable_names, &                                                   ! Field variable definition
             nodal_fieldvariables)      ! Output variables
 
+    else if ( element_identifier == 1002 ) then              ! Basic fully integrated 3D linear elastic element
+
+       call fieldvars_linelast_3d_Bbar(lmn, element_identifier, n_nodes, node_property_list, &         ! Input variables
+           n_properties, element_properties,element_coords, length_coord_array,  &                     ! Input variables
+            dof_increment, dof_total, length_dof_array,  &                                              ! Input variables
+            n_state_variables, initial_state_variables,updated_state_variables, &                       ! Input variables
+            n_field_variables,field_variable_names, &                                                   ! Field variable definition
+            nodal_fieldvariables)      ! Output variables
+
         else if ( element_identifier == 0 ) then
             call new_user_element_fieldvariables(lmn, element_identifier, n_nodes, node_property_list, &           ! Input variables
                 n_properties, element_properties,element_coords, length_coord_array, &                                   ! Input variables
@@ -241,6 +265,14 @@ subroutine user_element_fieldvariables(lmn, element_identifier, n_nodes, node_pr
 
  else if ( element_identifier == 101 ) then
             call fieldvars_linelast_2dbasic(lmn, element_identifier, n_nodes, node_property_list, &           ! Input variables
+                n_properties, element_properties,element_coords, length_coord_array, &                                   ! Input variables
+                dof_increment, dof_total, length_dof_array, &                                                            ! Input variables
+                n_state_variables, initial_state_variables,updated_state_variables, &                                    ! Input variables
+                n_field_variables,field_variable_names, &                                                                ! Field variable definition
+                nodal_fieldvariables)      ! Output variables
+
+ else if ( element_identifier == 102 ) then
+            call fieldvars_linelast_2d_Bbar(lmn, element_identifier, n_nodes, node_property_list, &           ! Input variables
                 n_properties, element_properties,element_coords, length_coord_array, &                                   ! Input variables
                 dof_increment, dof_total, length_dof_array, &                                                            ! Input variables
                 n_state_variables, initial_state_variables,updated_state_variables, &                                    ! Input variables
